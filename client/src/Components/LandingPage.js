ab5d9999c32748f2b2ac142d216afdaccb98a804
at-rooshee <rushania@youcanbook.me>
2019-05-15 08:14:50 +0300

    Adding file structure

---
+++ client/src/Components/LandingPage.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function LandingPage() {
+  return (
+    <div className="LandingPage">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <p>
+          Edit <code>src/LandingPage.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default LandingPage;
