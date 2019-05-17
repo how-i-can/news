2c6ad9b39ddf34b8b7cf5851f65d4f096d69e2b4
mash-a <mariya.emv@gmail.com>
2019-05-16 21:45:16 -0700

    Add Nav Bar

--- client/src/Components/LandingPage.js
+++ client/src/Components/LandingPage.js
@@ -1,22 +1,13 @@
-import React from 'react';
+import React from "react";
+import NavBar from "./NavBar";
 
 function LandingPage() {
   return (
     <div className="LandingPage">
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Edit <code>src/LandingPage.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <NavBar />
     </div>
   );
 }
