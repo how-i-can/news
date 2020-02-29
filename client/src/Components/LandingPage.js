370e0522d2596eb43bf796642f1d65d5e20e410f
Victoria Boggs <victoriaj13@gmail.com>
2020-02-29 17:03:16 -0500

    added test line to LandingPage.js for onboarding push to github

--- client/src/Components/LandingPage.js
+++ client/src/Components/LandingPage.js
@@ -9,6 +9,7 @@ import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import { withStyles } from "@material-ui/core/styles";
 
+
 const styles = theme => ({
   landingPage: {
     flexGrow: 1,
