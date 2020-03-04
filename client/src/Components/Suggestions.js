c98a5a9ff0d4576d876beeef80754fb683b22964
Mealear Khiev <mealear.khiev@critigen.com>
2020-03-03 18:09:17 -0800

    Adds search icon next to each search result

--- client/src/Components/Suggestions.js
+++ client/src/Components/Suggestions.js
@@ -5,6 +5,7 @@ import ListItem, { ListItemProps } from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
 import { withStyles } from "@material-ui/core/styles";
+import SearchIcon from "@material-ui/icons/Search";
 
 const styles = theme => ({
   searchList: {
@@ -21,6 +22,20 @@ const styles = theme => ({
   listItem: {
     flex: 1,
     fontWeight: 350
+  },
+  queryResult: {
+    display: "flex",
+    justifyContent: "space-evenly",
+    alignItems: "center"
+  },
+  queryLabel: {
+    flex: 4
+  },
+  searchIcon: {
+    width: theme.spacing.unit * 5,
+    marginLeft: "0.5em",
+    pointerEvents: "none",
+    flex: 0.25
   }
 });
 
@@ -40,13 +55,20 @@ class Suggestions extends Component {
             .join(`<b>${this.props.query.toLowerCase()}</b>`)
         };
         return (
-          <ListItem
-            key={article.url}
-            button
-            onClick={e => this.handleClick(e, article)}
-          >
-            <div className={classes.listItem} dangerouslySetInnerHTML={text} />
-          </ListItem>
+          <div className={classes.queryResult}>
+            <SearchIcon className={classes.searchIcon} />
+            <ListItem
+              className={classes.queryLabel}
+              key={article.url}
+              button
+              onClick={e => this.handleClick(e, article)}
+            >
+              <div
+                className={classes.listItem}
+                dangerouslySetInnerHTML={text}
+              />
+            </ListItem>
+          </div>
         );
       });
       return (
