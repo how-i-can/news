10b23687dc3e345967681ba17ac82e4caa0e2fc1
Mirha Masala <mirha.masala@me.com>
2020-03-20 20:18:09 +0800

    Clean up order of imports

--- client/src/Components/LandingPage.js
+++ client/src/Components/LandingPage.js
@@ -1,14 +1,15 @@
 import React, { Component } from "react";
+import BottomNavBar from "./BottomNavBar";
 import NewsCards from "./NewsCards";
+import NoResults from "./NoResults";
 import SearchBar from "./SearchBar";
 import Suggestions from "./Suggestions";
-import BottomNavBar from "./BottomNavBar";
-import PropTypes from "prop-types";
 import axios from "axios";
+import PropTypes from "prop-types";
+import Card from "@material-ui/core/Card";
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
 import { withStyles } from "@material-ui/core/styles";
-import NoResults from "./NoResults";
 
 const styles = theme => ({
   landingPage: {
