06065fa4841155c3c570d34c4a4b814e87fea23b
Mirha Masala <mirha.masala@me.com>
2020-04-21 17:22:01 +0800

    Refactor bottom nav bar

--- client/src/Components/BottomNavBar.js
+++ client/src/Components/BottomNavBar.js
@@ -4,30 +4,19 @@ import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 import Account from "@material-ui/icons/Person";
 import AppBar from "@material-ui/core/AppBar";
-import CssBaseline from "@material-ui/core/CssBaseline";
 import Grade from "@material-ui/icons/Grade";
 import Home from "@material-ui/icons/Home";
 import IconButton from "@material-ui/core/IconButton";
 import Toolbar from "@material-ui/core/Toolbar";
 
-const styles = theme => ({
+const styles = () => ({
   appBar: {
-    display: "flex",
-    position: "relative",
+    position: "unset",
     backgroundColor: "#87BCBF",
-    flexGrow: 1,
   },
-  toolbar: {
+  toolBar: {
     alignItems: "center",
     justifyContent: "space-evenly",
-  },
-  fabButton: {
-    position: "absolute",
-    zIndex: 1,
-    top: -30,
-    left: 0,
-    right: 0,
-    margin: "0 auto",
   },
 });
 
@@ -36,7 +25,7 @@ class BottomNavBar extends Component {
     const { classes } = this.props;
     return (
       <AppBar className={classes.appBar}>
-        <Toolbar className={classes.toolbar}>
+        <Toolbar className={classes.toolBar}>
           <IconButton onClick={this.props.loadDefaultNewsArticlesProp}>
             <Home />
           </IconButton>
