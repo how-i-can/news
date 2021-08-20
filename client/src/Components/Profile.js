33012d9c2819b7663fb2373005296c4e744ca441
michelleann815 <68318965+michelleann815@users.noreply.github.com>
2021-08-20 22:46:21 +0900

    Adds some styling to page

--- client/src/Components/Profile.js
+++ client/src/Components/Profile.js
@@ -1,20 +1,40 @@
-import React from "react";
+import React, { Component } from "react";
+import { withStyles } from "@material-ui/core/styles";
 
-const Profile = () => {
-  return (
-    <div>
-      <div>
-        <img
-          style={{ width: "160px", height: "160px", borderRadius: "80px" }}
-          alt=""
-          src="https://images.unsplash.com/photo-1625520306193-027951953dbf?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTkwfHxnaXJsfGVufDB8MnwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60"
-        />
-      </div>
-      <div>
-        <h4>Jessica Brown</h4>
-      </div>
-    </div>
-  );
-};
+const styles = () => ({
+  profilePhoto: {
+    display: "flex",
+    justifyContent: "center",
+    margin: 25,
+    width: 160,
+    height: 160,
+    borderRadius: 80,
+  },
+  userName: {
+    display: "inline-flex",
+    justifyContent: "center",
+    marginRight: "auto",
+    fontWeight: "bold",
+  },
+});
 
-export default Profile;
+class Profile extends Component {
+  render() {
+    const { classes } = this.props;
+    return (
+      <div>
+        <div>
+          <img
+            className={classes.profilePhoto}
+            alt=""
+            src="https://images.unsplash.com/photo-1625520306193-027951953dbf?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTkwfHxnaXJsfGVufDB8MnwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60"
+          />
+        </div>
+        <div>
+          <h4 className={classes.userName}>Jessica Brown</h4>
+        </div>
+      </div>
+    );
+  }
+}
+export default withStyles(styles)(Profile);
